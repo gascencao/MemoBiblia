20becd9cec47b85a5bf17e5920a587384dd78b5e
Gabriel David Ascencao <gascencao@gmail.com>
2016-08-01 13:40:30 -0500

    First draft for facebook login

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,3 +1,19 @@
+/*
+ * Para generar una key de android:
+ * keytool -genkey -v -keystore %HOMEPATH%\.android\debug.keystore -alias memobiblia_debug -keyalg RSA -keysize 2048 -validity 10000
+ * keytool -genkey -v -keystore %HOMEPATH%\.android\debug.keystore -alias memobiblia -keyalg RSA -keysize 2048 -validity 10000
+ * Va a solicitar una contraseña que está en el KeePass en el item Android KeyStore
+ * 
+ * Luego, se puede cargar la key en el sitio de phonegap de Adobe
+ * https://build.phonegap.com/apps/2097761/builds
+ * Va a solicitar una contraseña que está en el KeePass en el item Adobe Id
+ * 
+ * Por último, se puede exportar el hash para poner en facebook usando los comandos
+ * keytool -exportcert -alias memobiblia_debug -keystore %HOMEPATH%\.android\debug.keystore | "c:\Users\gascencao\.android\Open SSL\bin\openssl" sha1 -binary | "c:\Users\gascencao\.android\Open SSL\bin\openssl" base64
+ * keytool -exportcert -alias memobiblia -keystore %HOMEPATH%\.android\debug.keystore | "c:\Users\gascencao\.android\Open SSL\bin\openssl" sha1 -binary | "c:\Users\gascencao\.android\Open SSL\bin\openssl" base64
+ * Se guardan en la configuración de la App de Facebook, en la sección de la plataforma de Android en el input de hashes
+ * https://developers.facebook.com/apps/1565427663764053/settings/
+ */
 var app = {
 
     // Application Constructor
@@ -20,7 +36,28 @@ var app = {
     onDeviceReady: function() {
       $('.splash').hide();
       $('.main').show();
-      app.getVerse();
+
+      window.fbAsyncInit = function() {
+        if (window.cordova.platformId === "browser") {
+           facebookConnectPlugin.browserInit('1565427663764053');
+        }
+        $('.main').append('<a hred="#" class="facebook_login">Login</a><a hred="#" class="facebook_logout">Logout</a><span class="facebook_status">status</span>');
+        $('.facebook_login').click(function () {
+          facebookConnectPlugin.login([], function () {
+            $('.facebook_status').html('Success login');
+          }, function () {
+            console.log('Fail');
+          });
+        });
+        $('.facebook_logout').click(function () {
+          facebookConnectPlugin.logout(function () {
+            $('.facebook_status').html('Success logout');
+          }, function () {
+            console.log('Fail');
+          });
+        });
+      }
+      //app.getVerse();
     },
 
     getVerse: function() {
